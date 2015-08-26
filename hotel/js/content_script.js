689499c6018b413fce8ff3451baa0354182779c5
李军 <cashlzcj@gmail.com>
2015-08-26 20:26:46 +0800

    改进上架实现方式，加入没有配额的判断

--- hotel/js/content_script.js
+++ hotel/js/content_script.js
@@ -2,41 +2,13 @@
  * Created by junge on 15/8/21.
  */
 
-//chrome.tabs.onRequest.addListener(
-//    function(request, sender, sendResponse) {
-//        console.log(request.running);
-//        //console.log(sender.tab ?
-//        //"from a content script:" + sender.tab.url :
-//        //    "from the extension");
-//        //if (request.greeting == "hello")
-//        //    sendResponse({farewell: "goodbye"});
-//        //else
-//        //    sendResponse({}); // snub them.
-//        sendResponse({running: "start"});
-//    });
-
 window.addEventListener ("load", run_automatic, false);
-//window作用域下覆盖alert的方法
-//window.alert = function(str){
-//    console.log(str);
-//}
-
-//window.onbeforeunload = function(event){
-//    // 关闭
-//    if(event.clientX>document.body.clientWidth && event.clientY < 0 || event.altKey) {
-//        return "确定要关闭吗?";
-//    }else{ // 刷新
-//        return "你正在刷新页面";
-//    }
-//}
 
 var timerOut = null;
 function run_automatic (evt) {
     var jsInitChecktimer = setInterval (checkForJS_Finish, 100);
 
     function checkForJS_Finish () {
-        //var running = sessionStorage.running;
-        //console.log(running);
         var test4Arr = $(".test4select");
         if(test4Arr.length > 0) {
             clearInterval (jsInitChecktimer);
@@ -63,15 +35,15 @@ function run_automatic (evt) {
 function begin(array, index){
     clearTimerout(timerOut);
     if(index+1 > array.length){
-        //console.log("array count", array.length);
-        //console.log("index", index+1);
+        console.log("array count", array.length);
+        console.log("index", index+1);
         sessionStorage.currentIndex = 0;
         localStorage.total = Number(localStorage.total) + array.length;
         if($("a.next")[0]){
             $("a.next")[0].click();
             timerOut = setTimeout(function(){
                 location.reload();
-            },1000);
+            },4000);
         }
         else{
             // 结束了
@@ -83,22 +55,31 @@ function begin(array, index){
     }
 
     array[index].checked = true;
-    $("#id_batch_set_onsale").click();
-    timerOut = setTimeout(function(){
-        putaway(array, index);
-    },2000);
+    getRoomDetail(function (brs) {
+        if (brs) {
+            $("#id_batch_set_onsale").click();
+        }
+        else {
+            console.log("这个房型没有配额");
+        }
+
+        timerOut = setTimeout(function () {
+            putaway(array, index);
+        }, 2000);
+    });
 }
 
 // 上架
 function putaway(array, index){
     clearTimerout(timerOut);
-    $(".mlr")[0].click();
+    //$(".mlr")[0].click();
+    putawayRequest();
     sessionStorage.currentIndex = index + 1;
 
     // 10秒后进入下一轮
-    timerOut = setTimeout(function(){
-        location.reload();
-    }, 10000);
+    //timerOut = setTimeout(function(){
+    //    location.reload();
+    //}, 10000);
 }
 
 // 清理timeout
@@ -109,38 +90,69 @@ function clearTimerout(timer){
     }
 }
 
+// 上架请求
+function putawayRequest(){
+    var id = $(".test4select:checked").val();
+    $.ajax(
+        {
+            url:"http://mangocity.fangcang.com/HPMS/batchCommodityOnsale.shtml",
+            dataType:"json",
+            data:{
+                "commodityOnSaleVO.city":$("#cityCode").val(),
+                "commodityOnSaleVO.commodityIds":id,
+                "commodityOnSaleVO.isOnSale":1
+            },
+            success:function(result)
+            {
+                $('#popup_exit')[0].click();
+                timerOut = setTimeout(function(){
+                    location.reload();
+                }, 1000);
+            }
+        });
+}
 
-//function begin(item, callback){
-//    //console.log(item);
-//    item.checked = true;
-//    $("#id_batch_set_onsale").click();
-//    // 延迟1秒上架
-//    console.log("x1");
-//    setTimeout(function(){
-//        putaway(callback);
-//    },1000);
-//}
+// 获取房间数据
+function getRoomDetail(callback){
+    $.ajax({
+        url:"http://mangocity.fangcang.com/HPMS/hotelPriceDetailAction!toDayPriceDetail.shtml",
+        data:{
+            "commodityId":$(".test4select:checked").val(),
+            "cityCode":$("#cityCode").val()
+        },
+        dataType: "html",
+        type: "POST",
+        target:"#dayPriceDetailResult",
+        contentType: "application/x-www-form-urlencoded; charset=utf-8",
+        success:function(result){
+            callback(verifyRoomQuota($.makeArray($("#commodityQuotaList tr", $('<div>').append(result)))));
+        },
+        error:function(result){
+            callback(false);
+        }
+    });
+}
 
-// 上架
-//function putaway(callback){
-//    $(".mlr")[0].click();
-//    callback(null);
-//    //blooping = true;
-//    console.log("x2");
-//    //setTimeout(close,1000);
-//    //sleep(1500);
-//}
+function verifyRoomQuota(quotas){
+    for(var i=0; i<quotas.length; ++i){
+        var text = $("td:eq(6)", $(quotas[i])).text();
+        // 如果发现一个有存量的说明可以上
+        if(verifyNum(text.replace(/\n|\s/g, ''))){
+           return true;
+        }
+    }
 
-// 点 x 关闭
-//function close(){
-//    //blooping = false;
-//    //console.log(document.getElementById("popup_exit_1"));
-//    //console.log($("#popup_exit_1"));
-//    //return;
-//    //$("#popup_exit_1").click();
-//    console.log("x3");
-//}
-//
-//function sleep(d){
-//    for(var t = Date.now();Date.now() - t <= d;);
-//}
+    return false;
+}
+
+function verifyNum(str){
+    var array = str.split("/");
+    for(var i=0; i<array.length; ++i){
+        if(parseInt(array[i]) > 0){
+            //console.log(parseInt(array[i]));
+            return true;
+        }
+    }
+
+    return false;
+}
